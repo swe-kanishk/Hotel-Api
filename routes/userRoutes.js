64b6157d3d02e512beca041dfe425ea7f538535a
kanishk tiwari <kanishk.swe@gmail.com>
2024-08-10 13:29:52 +0530

    fix register route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,9 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { register, login, logout, addToFavourites } = require('../controllers/userController'); // Adjust path as per your actual file structure
 
-router.post('/register', (req, res) => {
-    console.log(req)
-});
+router.post('/register', register);
 router.post('/login', login);
 router.post('/logout', logout);
 router.post('favorites/add', addToFavourites);
